9149cb32bd01ee796a73e84088a5c89388275363
allysonsouza <allyson@hastedesign.com.br>
2016-10-10 18:35:33 -0300

    Refatoração do plugin e atualização de traduções

--- odin-toolkit.php
+++ odin-toolkit.php
@@ -16,24 +16,71 @@
  	exit;
  }
 
- // require_once get_template_directory() . '/inc/classes/abstracts/abstract-front-end-form.php';
- // require_once get_template_directory() . '/inc/classes/class-theme-options.php';
- // require_once get_template_directory() . '/inc/classes/class-options-helper.php';
- // require_once get_template_directory() . '/inc/classes/class-post-type.php';
- // require_once get_template_directory() . '/inc/classes/class-taxonomy.php';
- // require_once get_template_directory() . '/inc/classes/class-metabox.php';
- // require_once get_template_directory() . '/inc/classes/class-contact-form.php';
- // require_once get_template_directory() . '/inc/classes/class-post-form.php';
- // require_once get_template_directory() . '/inc/classes/class-user-meta.php';
- // require_once get_template_directory() . '/inc/classes/class-post-status.php';
- // require_once get_template_directory() . '/inc/classes/class-term-meta.php';
+if( ! class_exists( 'Odin_Toolkit' ) ) {
+
+	class Odin_Toolkit {
+		/**
+		 * Instance of this class.
+		 *
+		 * @var object
+		 */
+		protected static $instance = null;
+
+		/**
+		 * Plugin directory path
+		 *
+		 * @var string
+		 */
+		private $plugin_dir = null;
+
+		/**
+		 * Initialize the plugin.
+		 */
+		function __construct() {
+			$this->plugin_dir = plugin_dir_path( __FILE__ );
+			add_action( 'init', array( $this, 'load_textdomain' ) );
+			add_action( 'init', array( $this, 'require_classes' ), 0 );
+		}
+
+		/**
+		 * Return the plugin instance.
+		 */
+		public static function init()
+		{
+			// If the single instance hasn't been set, set it now.
+			if ( null == self::$instance ) {
+				self::$instance = new self;
+			}
+			return self::$instance;
+		}
+
+		/**
+		 * Require the plugin classes.
+		 */
+		public function require_classes() {
+			require_once $this->plugin_dir . '/inc/classes/abstracts/abstract-front-end-form.php';
+			require_once $this->plugin_dir . '/inc/classes/class-theme-options.php';
+			require_once $this->plugin_dir . '/inc/classes/class-options-helper.php';
+			require_once $this->plugin_dir . '/inc/classes/class-post-type.php';
+			require_once $this->plugin_dir . '/inc/classes/class-taxonomy.php';
+			require_once $this->plugin_dir . '/inc/classes/class-metabox.php';
+			require_once $this->plugin_dir . '/inc/classes/class-contact-form.php';
+			require_once $this->plugin_dir . '/inc/classes/class-post-form.php';
+			require_once $this->plugin_dir . '/inc/classes/class-user-meta.php';
+			require_once $this->plugin_dir . '/inc/classes/class-post-status.php';
+			require_once $this->plugin_dir . '/inc/classes/class-term-meta.php';
+		}
+
+		/**
+		 * Load plugin translation
+		 */
+		public function load_textdomain() {
+			load_plugin_textdomain( 'odin-toolkit', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+		}
+	}
+ }
 
  /**
-  * Carrega arquivos de traduções.
-  *
-  * @return void
-  */
- function odin_toolkit_load_textdomain() {
- 	load_plugin_textdomain( 'odin-toolkit', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
- }
- add_action( 'plugins_loaded', 'odin_toolkit_load_textdomain' );
+ * Initialize the plugin actions.
+ */
+add_action( 'plugins_loaded', array( 'Odin_Toolkit', 'init' ) );
