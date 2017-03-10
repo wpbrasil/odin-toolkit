fa9e46c6dbd12398a77fa3ba9564fe90e0a9368d
Allyson Souza <allyson@hastedesign.com.br>
2017-03-10 11:46:41 -0300

    Remove the settings options to choose modules that will be loaded, closes #1

--- odin-toolkit.php
+++ odin-toolkit.php
@@ -62,51 +62,31 @@ if( ! class_exists( 'Odin_Toolkit' ) ) {
 			require_once $this->plugin_dir . '/includes/classes/class-theme-options.php';
 			require_once $this->plugin_dir . '/includes/classes/class-options-helper.php';
 
-			//Odin Toolkit Settings
-			require_once $this->plugin_dir . '/includes/admin/class-settings.php';
-			Odin_Toolkit_Settings::init();
-
-			// Contact Form Module
-			if( Odin_Toolkit::module_active( 'contact_form_module' ) ) {
-				require_once $this->plugin_dir . '/includes/classes/abstracts/abstract-front-end-form.php';
-				require_once $this->plugin_dir . '/includes/classes/class-contact-form.php';
-			}
+			// Contact Form
+			require_once $this->plugin_dir . '/includes/classes/abstracts/abstract-front-end-form.php';
+			require_once $this->plugin_dir . '/includes/classes/class-contact-form.php';
 
 			// Metabox Module
-			if( Odin_Toolkit::module_active( 'metabox_module' ) ) {
-				require_once $this->plugin_dir . '/includes/classes/class-metabox.php';
-			}
+			require_once $this->plugin_dir . '/includes/classes/class-metabox.php';
 
 			// Post Form Module
-			if( Odin_Toolkit::module_active( 'post_form_module' ) ) {
-				require_once $this->plugin_dir . '/includes/classes/abstracts/abstract-front-end-form.php';
-				require_once $this->plugin_dir . '/includes/classes/class-post-form.php';
-			}
+			require_once $this->plugin_dir . '/includes/classes/abstracts/abstract-front-end-form.php';
+			require_once $this->plugin_dir . '/includes/classes/class-post-form.php';
 
 			// Post Status Module
-			if( Odin_Toolkit::module_active( 'post_status_module' ) ) {
-				require_once $this->plugin_dir . '/includes/classes/class-post-status.php';
-			}
+			require_once $this->plugin_dir . '/includes/classes/class-post-status.php';
 
 			// Post Type Module
-			if( Odin_Toolkit::module_active( 'post_type_module' ) ) {
-				require_once $this->plugin_dir . '/includes/classes/class-post-type.php';
-			}
+			require_once $this->plugin_dir . '/includes/classes/class-post-type.php';
 
 			// Taxonomy Module
-			if( Odin_Toolkit::module_active( 'taxonomy_module' ) ) {
-				require_once $this->plugin_dir . '/includes/classes/class-taxonomy.php';
-			}
+			require_once $this->plugin_dir . '/includes/classes/class-taxonomy.php';
 
 			// Term Meta Module
-			if( Odin_Toolkit::module_active( 'term_meta_module' ) ) {
-				require_once $this->plugin_dir . '/includes/classes/class-term-meta.php';
-			}
+			require_once $this->plugin_dir . '/includes/classes/class-term-meta.php';
 
 			// User Meta Module
-			if( Odin_Toolkit::module_active( 'user_meta_module' ) ) {
-				require_once $this->plugin_dir . '/includes/classes/class-term-meta.php';
-			}
+			require_once $this->plugin_dir . '/includes/classes/class-term-meta.php';
 		}
 
 		/**
@@ -114,16 +94,6 @@ if( ! class_exists( 'Odin_Toolkit' ) ) {
 		 */
 		public function load_textdomain() {
 			load_plugin_textdomain( 'odin-toolkit', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-		}
-
-		public static function module_active( $module ) {
-			$options = get_option( 'odin_toolkit_modules' );
-
-			if( ! empty( $options[ $module ] ) && $options[ $module ] == true ) {
-				return true;
-			} else {
-				return false;
-			}
 		}
 	}
  }
